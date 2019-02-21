765500f3adb886c223df335906ecbd0bf862f8f1
Mohit <mohit.kingra@ekt-digital.com>
2019-02-21 18:41:58 +0800

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Navigator from './navigator.js';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,19 +8,35 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
+          <span>
+	    Signed in as  
+	  </span>
+	  <a
             className="App-link"
-            href="https://reactjs.org"
+            href="#"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Member
+          </a>
+	  <span>
+	   ->
+	  </span>
+          <a
+            className="App-link"
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Sign out
           </a>
         </header>
+	<Navigator />
+        <footer className="App-footer">
+          <p>
+            Copyright Thoughtworks Inc.
+          </p>
+        </footer>
       </div>
     );
   }
