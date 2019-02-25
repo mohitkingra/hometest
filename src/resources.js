813a46885ced9c4fa0ca38ef3445ff55f59728cc
Mohit <mohit.kingra@ekt-digital.com>
2019-02-25 16:10:59 +0800

    refactor resource handling

--- src/resources.js
+++ src/resources.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './resources.css';
 
 let styles = {
 	click : {
@@ -25,24 +26,41 @@ class Resources extends Component {
   	})
   }
 
-  resourceSubmitted() {
-
+  addResource() {
   	let rsc = this.state.resources;
-  	rsc = rsc.concat(this.element.value);
+  	rsc = rsc.concat(this.refs.addresource.value);
   	this.setState({
   		resources : rsc
   	});
+
+  }
+
+  deleteResource() {
+    const el = this.element.parentElement;
+    el.style.display="none";
+
+    const delresource = el.innerText.substring(0, el.innerText.length - 1);
+
+    let rsc = this.state.resources;
+    rsc = rsc.filter(resource => resource !== delresource)
+    this.setState({
+      resources : rsc
+    })
+
   }
 
   render() {
 
   	let Resources = [];
 
-  	if(this.state.resources.length){
+  	if(this.state.resources.length > 0){
   		Resources = this.state.resources.map((resource, index) => {
   			return(
-  				<span>{resource}</span>
-  			);
+  				<div>
+            {resource}
+            <span className="close" onClick={this.deleteResource.bind(this)} ref={el => this.element = el}>x</span>
+  			  </div>
+        );
   		})
     }
 
@@ -52,8 +70,8 @@ class Resources extends Component {
          <a href="#" onClick={this.linkClicked.bind(this)}>
          	Specify Resources 
          </a>
-        <form onSubmit={this.resourceSubmitted.bind(this)} style={this.state.click? styles.clicked : styles.notclicked}>
-			<input type="text" ref={el => this.element = el} />
+        <form onSubmit={this.addResource.bind(this)} style={this.state.click? styles.clicked : styles.notclicked}>
+			<input type="text" ref="addresource" />
 			<input type="submit" />
 		</form>
 	 </div>
